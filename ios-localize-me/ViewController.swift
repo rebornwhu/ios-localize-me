79879b5a449d05a640bf26ddce6dc3775d34a6db
rebornwhu <rebornwhu@gmail.com>
2016-01-16 20:34:50 -0800

    Finish logic

--- ios-localize-me/ViewController.swift
+++ ios-localize-me/ViewController.swift
@@ -15,14 +15,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let locale = NSLocale.currentLocale()
+        let currentLangID = (NSLocale.preferredLanguages() as [String])[0]
+        let displayLang = locale.displayNameForKey(NSLocaleLanguageCode, value: currentLangID)
+        let capitalized = displayLang?.capitalizedStringWithLocale(locale)
+        localeLabel.text = capitalized
+        
+        labels[0].text = NSLocalizedString("LABEL_ONE", comment: "The numeber 1")
+        labels[1].text = NSLocalizedString("LABEL_TWO", comment: "The numeber 2")
+        labels[2].text = NSLocalizedString("LABEL_THREE", comment: "The numeber 3")
+        labels[3].text = NSLocalizedString("LABEL_FOUR", comment: "The numeber 4")
+        labels[4].text = NSLocalizedString("LABEL_FIVE", comment: "The numeber 5")
+        
+        let flagFile = NSLocalizedString("FLAG_FILE", comment: "Name of the flag")
+        flagImageView.image = UIImage(named: flagFile)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
